f16f64c8dd78b0592deeb3a13efdef9f2fc29d29
Austinet <austinetsplendour@gmail.com>
2023-10-14 16:35:19 +0100

    ch: fetch product, reduce bg-color of loading animation

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -27,7 +27,7 @@ const Product = () => {
   };
 
   useEffect(() => {
-    // getProduct();
+    getProduct();
   }, []);
 
   return (
@@ -43,7 +43,7 @@ const Product = () => {
               >
                 <svg
                   aria-hidden="true"
-                  className="w-[5rem] h-[5rem] mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                  className="w-[5rem] h-[5rem] mr-2 text-gray-200 animate-spin dark:text-gray-400 fill-blue-600"
                   viewBox="0 0 100 101"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
